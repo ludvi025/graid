231aaa3fd9c6b5bde1556fd952325359413a7de9
Adam Ludvik <rotestein@gmail.com>
2015-02-27 11:57:40 -0600

    Changed exporting gradebook to use new file.

--- lib/moodie/export_gradebook.py
+++ lib/moodie/export_gradebook.py
@@ -2,20 +2,23 @@ import os
 import csv
 from ..debug import db_exec_info, dbprint
 
-# Shall be the same as header of grade book
-FIELDS = 'user_id,moodle_id,first_name,last_name,grade,comments,grader'
+# Fields to take from each instance of `Record` in the `Gradebook`
+FIELDS = 'user_id,moodle_id,grade,comments,grader'
 FIELDS = FIELDS.split(',')
 
-MOODLE_FIELDS = 'User id,Moodle id,First name,Last name,Grade,Comments,Grader ID'
+# Header row for the exported gradebook, shall parallel FIELDS
+MOODLE_FIELDS = 'User id,Moodle id,Grade,Comments,Grader ID'
 MOODLE_FIELDS = MOODLE_FIELDS.split(',')
 
 assert len(FIELDS) == len(MOODLE_FIELDS)
 
+# Hack for outputting the header in the exported gradebook
 HEADER_ROW = {FIELDS[i]: MOODLE_FIELDS[i] for i in range(len(FIELDS))}
 
 # Shall be the same as header of user id to moodle id pairing file
 # and match the fields in FIELDS
-STUDENT_INFO_FIELDS = 'user_id,first_name,last_name'.split(',')
+TO_USER_ID_KEY = 'moodle_id'
+USER_ID_KEY = 'user_id'
 
 # Takes a Gradebook class and output file and creates CSV file ready to
 # be uploaded to moodle
@@ -25,11 +28,10 @@ def exportGradebook(grade_book, output_file, user_ids_path=None):
         uids = {}
         if user_ids_path != None:
             fin = open(user_ids_path)
-            for row in csv.DictReader(fin, STUDENT_INFO_FIELDS):
-
-                # TODO: Fix this. Too hacky
-                key = getNameKey(row['first_name'], row['last_name'])
-                uids[key] = row['user_id']
+            for row in csv.DictReader(fin, [TO_USER_ID_KEY, USER_ID_KEY]):
+                to_user_id = row[TO_USER_ID_KEY]
+                user_id = row[USER_ID_KEY]
+                uids[to_user_id] = user_id
 
             fin.close()
         dbprint(uids)
@@ -40,10 +42,13 @@ def exportGradebook(grade_book, output_file, user_ids_path=None):
 
         for record in grade_book:
             if user_ids_path != None:
-                key = getNameKey(record['first_name'], record['last_name'])
-                record['user_id'] = uids[key]
+                if record[TO_USER_ID_KEY] in user_ids_path:
+                    to_user_id = uids[record[TO_USER_ID_KEY]]
+                    record[USER_ID_KEY] = uids[to_user_id]
+                else:
+                    record[USER_ID] = 'xxxxx000'
             else:
-                record[USER_ID] = '#######'
+                record[USER_ID] = 'xxxxx000'
             cout.writerow(record)
 
         fout.close()
@@ -52,10 +57,3 @@ def exportGradebook(grade_book, output_file, user_ids_path=None):
     except:
         dbprint(db_exec_info())
         return False
-
-def getNameKey(first, last):
-    key = (last+first)
-    for c in ' \'\"':
-        key = key.replace(c, '')
-        key = key.lower()
-    return key
